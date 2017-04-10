c1e12ba82f8b72aedbdf494a650e001de21f7d3c
Matthias Zandanel <matthias.zandl@hotmail.com>
2017-04-10 16:56:43 +0200

    Projektdateien hinzufügen.

---
+++ WernerCAD/MainForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 12zanmat
+ * Date: 06.04.2017
+ * Time: 13:56
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace WernerCAD
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(13, 13);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(259, 47);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			this.openFileDialog1.Filter = "TAP-Dateien(*.tap)|*.tap";
+			this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.OpenFileDialog1FileOk);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.button1);
+			this.Name = "MainForm";
+			this.Text = "WernerCAD";
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ WernerCAD/MainForm.cs
@@ -0,0 +1,93 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 12zanmat
+ * Date: 06.04.2017
+ * Time: 13:56
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace WernerCAD
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		List<PointF> Punkte;
+		Matrix TransformationMatrix;
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void TransformPoints(PointF soll, PointF ist)
+		{
+			PointF[] points = Punkte.ToArray();
+			TransformationMatrix.TransformPoints(points);
+			TransformationMatrix.
+			
+		}
+		void OpenFileDialog1FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			try
+			{
+				using (StreamReader r = new StreamReader ( openFileDialog1.FileName ))
+				{
+					string line;
+					float X, Y;
+					
+					while ((line = r.ReadLine()) != null)
+					{
+						Match matchx = Regex.Match(line, @"X([0-9\.]+)");
+						Match matchy = Regex.Match(line, @"Y([0-9\.]+)");
+						
+						if ( matchx.Success )
+						{
+							String s = matchx.Groups[1].Value;
+							X = Convert.ToSingle(s);
+						}
+						
+						
+						
+						if ( matchy.Success )
+						{
+							String s = matchy.Groups[1].Value;
+							Y = Convert.ToSingle(s);
+						}
+						
+						if ( matchx.Success || matchy.Success )
+							Punkte.Add ( new PointF ( X, Y ) );
+					}
+				}
+			}
+			
+			catch ( Exception ex )
+			{
+				MessageBox.Show ( ex.ToString());
+			}
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			openFileDialog1.ShowDialog ();
+		}
+	}
+}
